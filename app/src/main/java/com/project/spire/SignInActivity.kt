57601bc920432a03f28f2b5427079690dce7d9d1
0jihunlee <0.jihun.lee@gmail.com>
2023-10-10 23:13:52 +0900

    Finished implementing Sign In and Sign Up activities

---
+++ app/src/main/java/com/project/spire/SignInActivity.kt
@@ -0,0 +1,26 @@
+package com.project.spire
+
+import android.content.Intent
+import android.os.Bundle
+
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.spire.databinding.ActivitySignInBinding
+
+
+class SignInActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySignInBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val signUpButton: TextView = binding.SignupTextBtn
+
+        signUpButton.setOnClickListener {
+            startActivity(Intent(this, SignUpActivity::class.java))
+        }
+
+    }
+}
