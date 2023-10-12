cb1feef1e2494a3ee4291e6785fb85ddda814eaa
sgeunhi <alsrb582@snu.ac.kr>
2023-10-12 23:50:03 +0900

    [feat] define user model

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -4,14 +4,15 @@ from app.models import Base
 from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column
 from app.models.guid import GUID
+from app.models.timestamp_mixin import TimestampMixin
 
-class User(Base):
+class User(Base, TimestampMixin):
     id: Mapped[UUID4] = mapped_column(GUID, primary_key=True, default=uuid.uuid4)
     username: Mapped[str] = mapped_column(String(100), nullable=False, unique=True, index=True)
-    # TDOD: Add email validation
     email: Mapped[str] = mapped_column(String(100), nullable=False, unique=True, index=True)
     profile_image_url: Mapped[str] = mapped_column(String(100), nullable=True)
     hashed_password: Mapped[str] = mapped_column(String(100), nullable=False)
+    is_super_user: Mapped[bool] = mapped_column(bool, nullable=False, default=False)
 
     class Config:
         orm_mode = True
