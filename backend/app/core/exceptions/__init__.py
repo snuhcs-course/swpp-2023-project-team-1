18a2cb3d42195fc8e4ae17fd3b98d624bbbbbb2e
sgeunhi <alsrb582@snu.ac.kr>
2023-10-13 01:33:20 +0900

    [feat] add base exceptions

---
+++ backend/app/core/exceptions/__init__.py
@@ -0,0 +1,15 @@
+from .token import *
+from .base import *
+
+__all__ = [
+    "DecodeTokenException",
+    "ExpiredTokenException",
+    "CustomException",
+    "BadRequestException",
+    "NotFoundException",
+    "ForbiddenException",
+    "UnauthorizedException",
+    "UnprocessableEntity",
+    "DuplicateValueException",
+    "ConflictException",
+]
