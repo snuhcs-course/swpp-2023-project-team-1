a13ce6fa7c8e6a2c9f86eac7e117ce883f7b06f2
sgeunhi <alsrb582@snu.ac.kr>
2023-10-12 23:50:47 +0900

    [feat] import models

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,10 +1,12 @@
 # Models for sqlalchemy
-
 from .base import Base
 from .user import User
-
+from .guid import GUID
+from .timestamp_mixin import TimestampMixin
 
 __all__ = [
     "Base",
     "User",
+    "GUID",
+    "TimestampMixin"
 ]
