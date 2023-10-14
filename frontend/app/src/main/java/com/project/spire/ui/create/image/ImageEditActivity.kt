9a2a39a6031200fde6bd82aec8d5bbb0fe72a1c3
Dongha Ahn <2019dahn@gmail.com>
2023-10-14 23:59:43 +0900

    [chore] created image mask package & file
    ui.create.image - image masking, prompting related ui
    
    ui.create.post - post uploading related ui

---
+++ frontend/app/src/main/java/com/project/spire/ui/create/image/ImageEditActivity.kt
@@ -0,0 +1,12 @@
+package com.project.spire.ui.create.image
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.spire.R
+
+class ImageEditActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_image_edit)
+    }
+}
