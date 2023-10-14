b932b00020d1ae038ededa07d854eb1181e085d3
sgeunhi <alsrb582@snu.ac.kr>
2023-10-14 15:49:41 +0900

    [feat] add code model for sqlalchemy

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -3,10 +3,12 @@ from .base import Base
 from .user import User
 from .guid import GUID
 from .timestamp_mixin import TimestampMixin
+from .code import Code
 
 __all__ = [
     "Base",
     "User",
     "GUID",
     "TimestampMixin"
+    "Code"
 ]
