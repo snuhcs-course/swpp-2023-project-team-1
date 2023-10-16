30c8a70b76ce7e1af70798cb84508d0dd7f9cfac
tictactoeid <jungjimin424@naver.com>
2023-10-16 16:35:37 +0900

    [feat] implemented image masking

--- frontend/app/src/main/java/com/project/spire/ui/create/image/ImageEditActivity.kt
+++ frontend/app/src/main/java/com/project/spire/ui/create/image/ImageEditActivity.kt
@@ -7,16 +7,66 @@ import android.widget.ImageView
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.example.spire.R
 import android.content.Intent
+import android.util.Log
+import android.view.MotionEvent
+import android.view.ScaleGestureDetector
+import android.widget.Button
+import android.widget.FrameLayout
 
 class ImageEditActivity : AppCompatActivity() {
-    private lateinit var constraintLayout: ConstraintLayout
+
+    private lateinit var mImageView: ImageView
+    private lateinit var mCanvasView: SpireCanvasView
+    private lateinit var eraseBtn: Button
+    private lateinit var resetBtn: Button
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_image_edit)
 
-
+        mImageView = findViewById(R.id.editingImage)
+        mCanvasView = findViewById(R.id.spireCanvasView)
+        mCanvasView.bindImage(mImageView)
+        eraseBtn = findViewById(R.id.eraseBtn)
+        eraseBtn.setOnClickListener {
+            if (mCanvasView.isErasing) mCanvasView.penMode()
+            else mCanvasView.eraseMode()
+        }
+        resetBtn = findViewById(R.id.resetBtn)
+        resetBtn.setOnClickListener {
+            mCanvasView.clearCanvas()
+        }
 
 
     }
+/*
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        Log.d("spireGesture", "activity-level touch detected")
+        mScaleDetector.onTouchEvent(event)
+        return true
+    }
+
+    inner class ScaleListener : ScaleGestureDetector.SimpleOnScaleGestureListener() {
+        override fun onScale(detector: ScaleGestureDetector): Boolean {
+            Log.d("spireGesture", "activity-level gesture detected")
+            mScaleFactor *= detector.scaleFactor
+
+            // Don't let the object get too small or too large.
+            mScaleFactor = Math.max(0.1f, Math.min(mScaleFactor, 5.0f))
+
+
+            //mImageView.scaleX = mScaleFactor
+            //mImageView.scaleY = mScaleFactor // TODO: apply to the viewgroup
+            //mImageView.invalidate()
+            //mCanvasView.scaleX = mScaleFactor
+            //mCanvasView.scaleY = mScaleFactor
+
+            //mFrameLayout.scaleX = mScaleFactor
+            //mFrameLayout.scaleY = mScaleFactor
+
+
+            return true
+        }
+    }*/
 }
--- frontend/app/src/main/java/com/project/spire/ui/create/image/SpireCanvasView.kt
+++ frontend/app/src/main/java/com/project/spire/ui/create/image/SpireCanvasView.kt
@@ -6,32 +6,118 @@ import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Path
-import android.os.Bundle
+import android.graphics.PorterDuff
+import android.graphics.PorterDuffXfermode
 import android.util.AttributeSet
+import android.util.Log
 import android.view.MotionEvent
+import android.view.ScaleGestureDetector
 import android.view.View
-import com.example.spire.R
-import kotlin.math.abs
+import android.widget.ImageView
+
+import java.io.Serializable
+
+data class PaintOptions(var color: Int = Color.BLUE, var strokeWidth: Float = 20f, var alpha: Int = 50, var xfermode: PorterDuffXfermode? = null) :
+    Serializable
 
 class SpireCanvasView(internal var context: Context, attrs: AttributeSet?) : View(context, attrs) {
+    private var paths = LinkedHashMap<Path, PaintOptions>()
+    private var paintOptions = PaintOptions()
+    private var currentX = 0f
+    private var currentY = 0f
+    private var startX = 0f
+    private var startY = 0f
+    // pinch zoom
+    private var mScaleFactor = 1f
+    private lateinit var mImageView: ImageView
+    var isErasing = false
+
+    private var previousScaleFactor = 1f
+    private var scaleCenterX = 0f
+    private var scaleCenterY = 0f
+    private var canvasTranslationX = 0f
+    private var canvasTranslationY = 0f
+
+    // drawing canvas
     private var mbitmap: Bitmap? = null
     private var mCanvas: Canvas? = null
     private var mPath: Path = Path()
     private var mPaint: Paint = Paint()
     private var mX: Float = 0.toFloat()
     private var mY: Float = 0.toFloat()
+/*
+    private var scaleListener = object : ScaleGestureDetector.SimpleOnScaleGestureListener() {
+        override fun onScale(detector: ScaleGestureDetector): Boolean {
+            mScaleFactor *= detector.scaleFactor
+            // Set min and max zoom levels.
+            //mScaleFactor = mScaleFactor.coerceAtLeast(0.2f).coerceAtMost(3.0f)
+            val deltaScale = mScaleFactor / previousScaleFactor
+            canvasTranslationX += (1 - deltaScale) * scaleCenterX
+            canvasTranslationY += (1 - deltaScale) * scaleCenterY
 
+            // Update pinch center
+            scaleCenterX = detector.focusX
+            scaleCenterY = detector.focusY
+
+            previousScaleFactor = mScaleFactor
+            invalidate()
+            return true
+        }
+    }
+    private val mScaleDetector = ScaleGestureDetector(context, scaleListener)
+*/
     init {
         mPaint.isAntiAlias = true
-        mPaint.color = Color.rgb(0, 0, 0)
+        //mPaint.color = Color.rgb(0, 0, 0)
         mPaint.style = Paint.Style.STROKE
         mPaint.strokeJoin = Paint.Join.ROUND
-        mPaint.strokeWidth = 4f
+        mPaint.strokeWidth = 20f
     }
 
     override fun onDraw(canvas: Canvas) {
+        Log.d("ERASE: ", isErasing.toString())
         super.onDraw(canvas)
+        // Apply translation based on pinch zoom
+        /*
+        canvas.translate(canvasTranslationX, canvasTranslationY)
+
+        // Apply scaling to the canvas
+        canvas.scale(mScaleFactor, mScaleFactor, scaleCenterX, scaleCenterY)
+        mImageView.translationX = canvasTranslationX
+        mImageView.translationY = canvasTranslationY
+        mImageView.scaleX = mScaleFactor
+        mImageView.scaleY = mScaleFactor
+*/
+        for ((path, options) in paths) {
+            mPaint.color = options.color
+            mPaint.xfermode = options.xfermode
+            mPaint.strokeWidth = options.strokeWidth
+            if (options.xfermode != null) {
+                //mPaint.xfermode = PorterDuffXfermode(PorterDuff.Mode.CLEAR)
+                //paint.xfermode = PorterDuffXfermode(PorterDuff.Mode.SRC_OUT)
+                //mPaint.color = Color.RED
+                setLayerType(LAYER_TYPE_HARDWARE, null)
+                //mPaint.strokeWidth = 50f
+            }
+            canvas.drawPath(path, mPaint)
+
+        }
+        mPaint.color = paintOptions.color
+        mPaint.xfermode = paintOptions.xfermode
+        mPaint.strokeWidth = paintOptions.strokeWidth
+        if (isErasing) {
+            setLayerType(LAYER_TYPE_HARDWARE, null)
+            //
+            //paint.xfermode = PorterDuffXfermode(PorterDuff.Mode.SRC_OUT)
+            //mPaint.color = Color.RED
+            //mPaint.strokeWidth = 50f
+        } else {
+            //mPaint.strokeWidth = 20f
+            mPaint.color = paintOptions.color
+        }
+
         canvas.drawPath(mPath, mPaint)
+
     }
 
     override fun onSizeChanged(w: Int, h: Int, oldw:Int, oldh: Int) {
@@ -40,54 +126,70 @@ class SpireCanvasView(internal var context: Context, attrs: AttributeSet?) : Vie
         mCanvas = Canvas(mbitmap!!)
     }
 
-    private fun onStartTouchEvent(x: Float, y: Float) {
-        mPath.moveTo(x, y)
-        mX = x
-        mY = y
-    }
-
-    private fun onMoveTouchEvent(x: Float,y: Float) {
-        val dx = abs(x - mX)
-        val dy = abs(y - mY)
-        if (dx >= TOLEARANCE || dy >= TOLEARANCE) {
-            mPath.quadTo(mX, mY, (x+mX)/2, (y+mY)/2)
-            mX = x
-            mY = y
-        }
-    }
-
-    private fun upTouchEvent() {
-        mPath.lineTo(mX, mY)
-    }
-
     fun clearCanvas() {
         mPath.reset()
+        paths = LinkedHashMap()
+        penMode()
         invalidate()
     }
 
-    override fun onTouchEvent(event: MotionEvent?): Boolean {
-        val x = event!!.x
+    fun bindImage(imageView: ImageView) {
+        mImageView = imageView
+    }
+    fun eraseMode() {
+        isErasing = true
+        paintOptions.color = Color.RED
+        paintOptions.xfermode = PorterDuffXfermode(PorterDuff.Mode.CLEAR)
+        paintOptions.strokeWidth = 50f
+    }
+
+    fun penMode() {
+        isErasing = false
+        paintOptions.color = Color.BLUE
+        paintOptions.xfermode = null
+        paintOptions.strokeWidth = 20f
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        Log.d("spireGesture", "canvasView-level gesture detected")
+
+       //mScaleDetector.onTouchEvent(event)
+        val x = event.x // about drawing
         val y = event.y
-        when(event.action) {
-            MotionEvent.ACTION_DOWN->{
-                onStartTouchEvent(x, y)
-                invalidate()
-            }
-            MotionEvent.ACTION_MOVE -> {
-                onMoveTouchEvent(x, y)
-                invalidate()
-            }
-            MotionEvent.ACTION_UP -> {
-                upTouchEvent()
-                invalidate()
+        if (event.pointerCount == 1) {
+            when(event.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    startX = x
+                    startY = y
+                    mPath.reset()
+                    mPath.moveTo(x, y)
+                    currentX = x
+                    currentY = y
+                    invalidate()
+                }
+                MotionEvent.ACTION_MOVE -> {
+                    mPath.quadTo(currentX, currentY, (x + currentX) / 2, (y + currentY) / 2)
+                    currentX = x
+                    currentY = y
+                    invalidate()
+                }
+                MotionEvent.ACTION_UP -> {
+                    mPath.lineTo(currentX, currentY)
+
+                    // draw a dot on click
+                    if (startX == currentX && startY == currentY) {
+                        mPath.lineTo(currentX, currentY + 2)
+                        mPath.lineTo(currentX + 1, currentY + 2)
+                        mPath.lineTo(currentX + 1, currentY)
+                    }
+
+                    paths[mPath] = paintOptions
+                    mPath = Path()
+                    paintOptions = PaintOptions(paintOptions.color, paintOptions.strokeWidth, paintOptions.alpha, paintOptions.xfermode)
+                    invalidate()
+                }
             }
         }
         return true
     }
-
-    companion object {
-        private val TOLEARANCE = 5f
-    }
-
-
 }
