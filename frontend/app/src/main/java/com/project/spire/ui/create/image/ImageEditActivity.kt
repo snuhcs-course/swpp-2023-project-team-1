dd1fbdadd87b6ce393eb74b4ab7387bae1621a23
tictactoeid <jungjimin424@naver.com>
2023-10-19 12:06:41 +0900

    [feat] implemented prompt input and edited xml

--- frontend/app/src/main/java/com/project/spire/ui/create/image/ImageEditActivity.kt
+++ frontend/app/src/main/java/com/project/spire/ui/create/image/ImageEditActivity.kt
@@ -11,33 +11,46 @@ import android.util.Log
 import android.view.MotionEvent
 import android.view.ScaleGestureDetector
 import android.widget.Button
+import android.widget.EditText
 import android.widget.FrameLayout
+import android.widget.ImageButton
 
 class ImageEditActivity : AppCompatActivity() {
 
     private lateinit var mImageView: ImageView
     private lateinit var mCanvasView: SpireCanvasView
-    private lateinit var eraseBtn: Button
-    private lateinit var resetBtn: Button
+    private lateinit var eraseBtn: ImageButton
+    private lateinit var resetBtn: ImageButton
+    private lateinit var promptSuggestBtn: Button
+    private lateinit var promptInput: EditText
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_image_edit)
-
-        mImageView = findViewById(R.id.editingImage)
-        mCanvasView = findViewById(R.id.spireCanvasView)
+        mImageView = findViewById(R.id.editing_image)
+        mCanvasView = findViewById(R.id.spire_canvas_view)
         mCanvasView.bindImage(mImageView)
-        eraseBtn = findViewById(R.id.eraseBtn)
+        eraseBtn = findViewById(R.id.erase_button)
         eraseBtn.setOnClickListener {
             if (mCanvasView.isErasing) mCanvasView.penMode()
             else mCanvasView.eraseMode()
         }
-        resetBtn = findViewById(R.id.resetBtn)
+        // TODO: change icon
+        resetBtn = findViewById(R.id.reset_button)
         resetBtn.setOnClickListener {
             mCanvasView.clearCanvas()
         }
 
+        promptSuggestBtn = findViewById(R.id.prompt_suggestion_button)
+        promptInput = findViewById(R.id.prompt_input)
+        promptSuggestBtn.setOnClickListener {
+            val currentText = promptInput.text.toString()
+            if (currentText == "") promptInput.setText(promptSuggestBtn.text)
+            else {
+                promptInput.setText(currentText + ", " + promptSuggestBtn.text.toString())
+            }
+        }
 
     }
 /*
