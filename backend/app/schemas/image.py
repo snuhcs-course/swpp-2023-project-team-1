51f01fb8dec3f5afebb5a226601829748751f93d
HyungGeun-Cho <chris1503@naver.com>
2023-10-26 14:44:59 +0900

    [feat] add image testing image generation api

--- backend/app/schemas/image.py
+++ backend/app/schemas/image.py
@@ -3,7 +3,15 @@ from datetime import datetime
 from pydantic import UUID4, BaseModel, ConfigDict, Field
 
 class ImageBase(BaseModel):
-    base64_image: str = Field(..., description="Base64 Image")
+    image_base64: str = Field(..., description="Base64 Image")
 
 class ImageUploadResponse(BaseModel):
     image_url: str = Field(..., description="Image S3 url")
+
+
+class ImageGenerate(BaseModel):
+    image_original_base64: str = Field(..., description="Original Image Base64")
+    image_mask_base64: str = Field(..., description="Mask Image Base64")
+
+class ImageGenerateResponse(BaseModel):
+    image_generated_url: str = Field(..., description="Generated Image S3 url")
--- backend/app/services/image_service.py
+++ backend/app/services/image_service.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 import boto3
 import base64
 from app.core import config
-
+import httpx
 
 class ImageService:
 
@@ -24,14 +24,15 @@ class ImageService:
             print("s3 bucket connected!") 
             self.s3 = s3
 
+
     @Transactional()
-    async def upload_image(self, image_id: UUID4, base64_image: str, file_extension: str = "jpg", **kwargs):
+    async def upload_image(self, image_id: UUID4, image_base64: str, image_type: str ="", file_extension: str = "jpg", **kwargs):
         try:
             # Decode the base64 image
-            image_bytes = base64.b64decode(base64_image)
+            image_bytes = base64.b64decode(image_base64)
 
             # Define the key (path) for the image in the S3 bucket
-            key = f"images/{image_id}.{file_extension}"
+            key = f"images/{image_id}_{image_type}.{file_extension}"
 
             # Upload the image to the S3 bucket
             self.s3.put_object(
@@ -52,4 +53,50 @@ class ImageService:
             raise e
 
 
-   
+    async def generate_image(self, image_id: UUID4, image_original_base64: str, image_mask_base64: str, timeout: int = 30):
+        INFERENCE_SERVER_URL = "http://0.0.0.0:8000/api/image/test"
+        # 1. Send the base64 encoded image to the inference server
+        async with httpx.AsyncClient() as client:
+            try:
+                response = await client.post(
+                    INFERENCE_SERVER_URL,
+                    json={
+                        "image_original_base64": image_original_base64,
+                        "image_mask_base64": image_mask_base64
+                    },
+                    # headers=self.HEADERS,  # Uncomment if you have headers
+                    timeout=timeout
+                )
+                print("ok")
+                response.raise_for_status()  # Raise an exception for HTTP errors
+                
+                image_generated_base64 = response.json().get("image_original_base64")
+
+                if not image_generated_base64:
+                    raise ValueError("Invalid response from inference server")
+            except Exception as e:
+                raise e
+
+        await self.upload_image(
+            image_id=image_id,
+            image_base64=image_original_base64,
+            image_type = "original"
+        )
+
+        await self.upload_image(
+            image_id=image_id,
+            image_base64=image_mask_base64,
+            image_type = "mask"
+        )
+
+        # 2. Upload the recreated image to S3
+        image_generated_url = await self.upload_image(
+            image_id=image_id,
+            image_base64=image_generated_base64,
+            image_type = "generated"
+        )
+
+        return image_generated_url
+    
+
+
