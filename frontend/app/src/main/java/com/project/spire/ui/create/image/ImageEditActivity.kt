dc05f292016ce3cc2b733ee49f7bb1490248b26f
tictactoeid <jungjimin424@naver.com>
2023-10-27 18:43:46 +0900

    [feat] implemented write text activity

--- frontend/app/src/main/java/com/project/spire/ui/create/image/ImageEditActivity.kt
+++ frontend/app/src/main/java/com/project/spire/ui/create/image/ImageEditActivity.kt
@@ -1,6 +1,7 @@
 package com.project.spire.ui.create.image
 
 import android.content.ContentValues
+import android.content.Intent
 import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Build
@@ -33,6 +34,7 @@ class ImageEditActivity : AppCompatActivity() {
     private lateinit var resetBtn: ImageButton
     private lateinit var promptSuggestBtn: Button
     private lateinit var promptInput: EditText
+    private lateinit var nextBtn: Button
 
 
     @RequiresApi(Build.VERSION_CODES.TIRAMISU)
@@ -111,62 +113,49 @@ class ImageEditActivity : AppCompatActivity() {
             }
         }
 
-        var saveButton = binding.saveButton
-        saveButton.setOnClickListener {
-            //saveImageOnAboveAndroidQ(mCanvasView.getBitmap())
-            //saveImageOnAboveAndroidQ(mCanvasView.getBitmap(mImageView.width, mImageView.height))
-            saveImageOnAboveAndroidQ(mCanvasView.getBitmap()) // TODO: 긴쪽 * 긴쪽의 정사각형으로 나오는듯?
+        nextBtn = binding.nextButton
+        nextBtn.setOnClickListener {
+            val intent = Intent(this, WriteTextActivity::class.java)
+            startActivity(intent)
+            // finish()
         }
 
-    }
+        // TODO: implement toolbar buttons
 
 
-    //Android Q (Android 10, API 29 이상에서는 이 메서드를 통해서 이미지를 저장한다.)
-    //@RequiresApi(Build.VERSION_CODES.Q)
-    private fun saveImageOnAboveAndroidQ(bitmap: Bitmap) {
-        // TODO: crop image to square
-        val fileName = System.currentTimeMillis().toString() + ".png" // 파일이름 현재시간.png
-
-        /*
-        * ContentValues() 객체 생성.
-        * ContentValues는 ContentResolver가 처리할 수 있는 값을 저장해둘 목적으로 사용된다.
-        * */
-        val contentValues = ContentValues()
-        contentValues.apply {
-            put(MediaStore.Images.Media.RELATIVE_PATH, "DCIM/ImageSave") // 경로 설정
-            put(MediaStore.Images.Media.DISPLAY_NAME, fileName) // 파일이름을 put해준다.
-            put(MediaStore.Images.Media.MIME_TYPE, "image/png")
-            put(MediaStore.Images.Media.IS_PENDING, 1) // 현재 is_pending 상태임을 만들어준다.
-            // 다른 곳에서 이 데이터를 요구하면 무시하라는 의미로, 해당 저장소를 독점할 수 있다.
-        }
-        // val contentResolver = context.getContentResolver()
-
-        // 이미지를 저장할 uri를 미리 설정해놓는다.
-        val uri =
-            contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
-
-        try {
-            if (uri != null) {
-                val image = contentResolver.openFileDescriptor(uri, "w", null)
-                // write 모드로 file을 open한다.
-
-                if (image != null) {
-                    val fos = FileOutputStream(image.fileDescriptor)
-                    bitmap.compress(Bitmap.CompressFormat.PNG, 100, fos)
-                    //비트맵을 FileOutputStream를 통해 compress한다.
-                    fos.close()
-
-                    contentValues.clear()
-                    contentValues.put(MediaStore.Images.Media.IS_PENDING, 0) // 저장소 독점을 해제한다.
-                    contentResolver.update(uri, contentValues, null, null)
-                }
+        fun saveImageOnAboveAndroidQ(bitmap: Bitmap) {
+            val fileName = System.currentTimeMillis().toString() + ".png"
+            val contentValues = ContentValues()
+            contentValues.apply {
+                put(MediaStore.Images.Media.RELATIVE_PATH, "DCIM/ImageSave")
+                put(MediaStore.Images.Media.DISPLAY_NAME, fileName)
+                put(MediaStore.Images.Media.MIME_TYPE, "image/png")
+                put(MediaStore.Images.Media.IS_PENDING, 1)
             }
-        } catch (e: FileNotFoundException) {
-            e.printStackTrace()
-        } catch (e: IOException) {
-            e.printStackTrace()
-        } catch (e: Exception) {
-            e.printStackTrace()
+
+            val uri =
+                contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+
+            try {
+                if (uri != null) {
+                    val image = contentResolver.openFileDescriptor(uri, "w", null)
+                    if (image != null) {
+                        val fos = FileOutputStream(image.fileDescriptor)
+                        bitmap.compress(Bitmap.CompressFormat.PNG, 100, fos)
+                        fos.close()
+
+                        contentValues.clear()
+                        contentValues.put(MediaStore.Images.Media.IS_PENDING, 0)
+                        contentResolver.update(uri, contentValues, null, null)
+                    }
+                }
+            } catch (e: FileNotFoundException) {
+                e.printStackTrace()
+            } catch (e: IOException) {
+                e.printStackTrace()
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
         }
     }
 }
---
+++ frontend/app/src/main/java/com/project/spire/ui/create/image/WriteTextActivity.kt
@@ -0,0 +1,22 @@
+package com.project.spire.ui.create.image
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.spire.R
+import com.example.spire.databinding.ActivityWriteTextBinding
+
+class WriteTextActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityWriteTextBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
+
+        binding = ActivityWriteTextBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val appbar = binding.writeTextAppBarLayout
+        appbar.toolbarText.setText(R.string.title_toolbar_write_text)
+
+
+    }
+}
