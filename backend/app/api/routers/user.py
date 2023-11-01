5a15d772b3dc4dd4ed660ac866b1beb3e5126ac8
HyungGeun-Cho <chris1503@naver.com>
2023-11-02 00:32:24 +0900

    [feat] add accept follow request api

--- backend/app/api/routers/user.py
+++ backend/app/api/routers/user.py
@@ -15,6 +15,58 @@ from app.schemas.user import (
 )
 from app.session import get_db_transactional_session
 from sqlalchemy.ext.asyncio import AsyncSession
+from app.services.user_service import UserService
 
 
 user_router = APIRouter()
+
+@user_router.post(
+    "/{user_id}/follow",
+    summary="Request user follow",
+    description="Request user follow",
+    dependencies=[Depends(PermissionDependency([IsAuthenticated]))],
+)
+async def follow_user(
+    req: Request,
+    user_id: UUID4,
+):
+    user_svc = PostService()
+    follow = await user_svc.follow_user(
+        followed_user_id=user_id, 
+        following_user_id = req.user.id
+    )
+    return {"message": f"Requested user {user_id} follow"}
+
+@user_router.delete(
+    "/{user_id}/unfollow",
+    summary="Unfollow user",
+    description="Unfollow user",
+    dependencies=[Depends(PermissionDependency([IsAuthenticated]))],
+)
+async def unfollow_user(
+    req: Request,
+    user_id: UUID4,
+):
+    user_svc = PostService()
+    follow = await user_svc.unfollow_user(
+        followed_user_id=user_id, 
+        following_user_id = req.user.id
+    )
+    return {"message": f"Unfollowed user {user_id}"}
+
+@user_router.post(
+    "/{user_id}/accept",
+    summary="Accept user follow request",
+    description="Accept user follow request",
+    dependencies=[Depends(PermissionDependency([IsAuthenticated]))],
+)
+async def accept_user_follow(
+    req: Request,
+    user_id: UUID4,
+):
+    user_svc = PostService()
+    follow = await user_svc.unfollow_user(
+        followed_user_id=req.user.id,
+        following_user_id=user_id
+    )
+    return {"message": f"Accepted user {user_id} follow request"}
