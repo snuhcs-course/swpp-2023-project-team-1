cb7d77278aaa0c9c3ee2b1980846597e7359e8b4
HyungGeun-Cho <chris1503@naver.com>
2023-11-03 23:51:18 +0900

    [feat] implement follow related services

--- backend/app/repository/user.py
+++ backend/app/repository/user.py
@@ -5,6 +5,8 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import and_, delete, select, func, case, update
 from sqlalchemy.orm import with_expression, selectinload, contains_eager
 from app.models.user import User, Follow
+from sqlalchemy.exc import IntegrityError, NoResultFound
+from app.core.exceptions.user import FollowAlreadyExistsException
 
 @Transactional()
 async def get_follow_by_user_ids(following_user_id: int, followed_user_id: int, session: AsyncSession) -> Follow | None:
@@ -24,6 +26,9 @@ async def create_follow(following_user_id: int, followed_user_id: int, session: 
         # FIXME: mypy
         follow = Follow(following_user_id=following_user_id, followed_user_id=followed_user_id, accept_status=0)  # type: ignore
         session.add(follow)
+
+    else:
+        raise FollowAlreadyExistsException()
     
     await session.commit()
     await session.refresh(follow)
@@ -37,16 +42,22 @@ async def update_follow(following_user_id: int, followed_user_id: int, session: 
     if follow is not None:
         # FIXME: mypy
         follow.accept_status = 1
-        follow = Follow(following_user_id=following_user_id, followed_user_id=followed_user_id, accept_status=1)  # type: ignore
-    
-    await session.commit()
-    await session.refresh(follow)
+        await session.commit()
+    else:
+        raise NoResultFound()
 
     return follow
 
 @Transactional()
 async def delete_follow(following_user_id: int, followed_user_id: int, session: AsyncSession):
-    stmt = delete(Follow).where(Follow.following_user_id == following_user_id, Follow.followed_user_id)
+    follow = await get_follow_by_user_ids(following_user_id, followed_user_id, session=session)
+    stmt = delete(Follow).where(Follow.following_user_id == following_user_id, Follow.followed_user_id == followed_user_id)
+    await session.execute(stmt)
+    return
+
+@Transactional()
+async def query_user_by_user_name(search_string: int, session: AsyncSession):
+    stmt = delete(Follow).where(Follow.following_user_id == following_user_id, Follow.followed_user_id == followed_user_id)
     await session.execute(stmt)
     return
 
--- backend/app/services/user_service.py
+++ backend/app/services/user_service.py
@@ -4,7 +4,7 @@ from app.session import Transactional
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.models.user import User, Follow
 from app.schemas.user import FollowBase
-from app.core.exceptions.user import UserNotFoundException, FollowNotFoundException
+from app.core.exceptions.user import UserNotFoundException, FollowNotFoundException, FollowMyselfException, FollowAlreadyAcceptedException, FollowWrongStatusException
 from app.repository import user
 from sqlalchemy.exc import IntegrityError, NoResultFound
 from app.core.exceptions.base import (
@@ -26,48 +26,66 @@ class UserService:
             return False
 
         return True
+    
+    @Transactional()
+    async def get_follow_by_user_ids(self, following_user_id: UUID4, followed_user_id: UUID4, session: AsyncSession) -> Follow:
+        try:
+            return await user.get_follow_by_user_ids(following_user_id, followed_user_id)
+        
+        except NoResultFound as e:
+            raise FollowNotFoundException from e
 
     @Transactional()
-    async def follow_user(self, following_user_id: UUID4, followed_user_id: UUID4, session: AsyncSession) -> Follow:
+    async def request_follow(self, following_user_id: UUID4, followed_user_id: UUID4, session: AsyncSession) -> Follow:
+        if following_user_id == followed_user_id:
+            raise FollowMyselfException
+
         try:
             follow_obj = await user.create_follow(following_user_id=following_user_id, followed_user_id=followed_user_id)
             return follow_obj
         
         except IntegrityError as e:
             raise BadRequestException(str(e.orig)) from e
-    
-    # @Transactional()
-    # async def get_follow_by_user_ids(self, following_user_id: UUID4, followed_user_id: UUID4, session: AsyncSession, **kwargs) -> Post:
-    #     try:
-    #         return await follw.get_follow_by_user_ids(following_user_id, followed_user_id)
-        
-    #     except NoResultFound as e:
-    #         raise FollowNotFoundException from e
 
     @Transactional()
     async def accept_follow_request(self, following_user_id: UUID4, followed_user_id: UUID4, session: AsyncSession) -> Follow:
+        try:
+            follow_obj = await self.get_follow_by_user_ids(following_user_id, followed_user_id)
+        except NoResultFound as e:
+            raise FollowNotFoundException from e
+        
+        if follow_obj.accept_status == 1:
+            raise FollowAlreadyAcceptedException
+            
         try:
             follow_obj = await user.update_follow(following_user_id, followed_user_id)
             return follow_obj
 
         except NoResultFound as e:
             raise FollowNotFoundException from e
-
+    
     @Transactional()
-    async def reject_follow_request(self, following_user_id: UUID4, followed_user_id: UUID4, session: AsyncSession):
+    async def delete_follow(self, following_user_id: UUID4, followed_user_id: UUID4, accept_status: int, session: AsyncSession) -> Follow:
         try:
-            await user.delete_follow(following_user_id, followed_user_id)
-
+            follow_obj = await self.get_follow_by_user_ids(following_user_id, followed_user_id)
         except NoResultFound as e:
             raise FollowNotFoundException from e
-   
-    @Transactional()
-    async def unfollow_user(self, following_user_id: UUID4, followed_user_id: UUID4, session: AsyncSession):
+
+        if follow_obj.accept_status != accept_status:
+            raise FollowWrongStatusException
+
         try:
             await user.delete_follow(following_user_id, followed_user_id)
-
         except NoResultFound as e:
             raise FollowNotFoundException from e
+
+    @Transactional()
+    async def search_user(self, search_string: str, session: AsyncSession):
+        try:
+            await user.query_user_by_user_name(search_string)
+
+        except IntegrityError as e:
+            raise BadRequestException(str(e.orig)) from e
 
 
 async def get_my_info_by_id(user_id: UUID4, session: AsyncSession) -> User:
