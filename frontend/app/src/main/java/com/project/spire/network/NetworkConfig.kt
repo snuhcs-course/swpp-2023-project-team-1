a090e23f26bfe200ec5b3c17c7edcda029816635
Dongha Ahn <2019dahn@gmail.com>
2023-11-07 22:20:50 +0900

    [fix] deleted network config

--- frontend/app/src/main/java/com/project/spire/network/NetworkConfig.kt
+++
@@ -1,8 +0,0 @@
-package com.project.spire.network
-
-// Secret URLs
-// Must be gitignored
-object NetworkConfig {
-    const val SPIRE_URL = "http://52.78.87.8:8000/api/"
-    const val INFERENCE_URL = "http://147.46.15.75:32317/v2/"
-}
