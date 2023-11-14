e79fe886b01e387fe25342f51f4a4dabca3a60c0
Dongha Ahn <2019dahn@gmail.com>
2023-11-14 20:07:36 +0900

    [fix] keeps feed status while navigating

--- frontend/app/src/main/java/com/project/spire/ui/feed/FeedFragment.kt
+++ frontend/app/src/main/java/com/project/spire/ui/feed/FeedFragment.kt
@@ -3,6 +3,7 @@ package com.project.spire.ui.feed
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -12,10 +13,12 @@ import androidx.navigation.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.spire.databinding.FragmentFeedBinding
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
 
 class FeedFragment : Fragment() {
     private var _binding: FragmentFeedBinding? = null
-    private var postAdapter: FeedAdapter? = null
+    private lateinit var feedViewModel: FeedViewModel
 
     private val binding get() = _binding!!
     private lateinit var recyclerView: RecyclerView
@@ -25,17 +28,16 @@ class FeedFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentFeedBinding.inflate(inflater, container, false)
+        if (_binding == null) {
+            _binding = FragmentFeedBinding.inflate(inflater, container, false)
+            feedViewModel = ViewModelProvider(this)[FeedViewModel::class.java]
+            feedViewModel.getInitialPosts()
+            Log.d("FeedFragment", "Loaded initial posts")
+        }
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-
-        val feedViewModel = ViewModelProvider(this)[FeedViewModel::class.java]
-
-        feedViewModel.getInitialPosts()
-
-        binding.appBar.outlineProvider = null
 
         recyclerView = binding.recyclerViewFeed
         val linearLayoutManager = LinearLayoutManager(context)
@@ -51,22 +53,19 @@ class FeedFragment : Fragment() {
                     adapter = FeedAdapter(it, context, findNavController())
                     binding.shimmerViewContainer.stopShimmer()
                     binding.shimmerViewContainer.visibility = View.GONE
-
                 }
             }
         }
 
+        binding.appBar.outlineProvider = null
         binding.swipeRefreshLayout.setOnRefreshListener {
             feedViewModel.getInitialPosts()
             binding.swipeRefreshLayout.isRefreshing = false
         }
-
-        recyclerView.adapter = postAdapter
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
-        _binding = null
     }
 
     private val onChildAttachStateChangeListener = object : RecyclerView.OnChildAttachStateChangeListener {
