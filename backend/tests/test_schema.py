642142ef48fa93ac9a0ad65b46022881851775d5
HyungGeun-Cho <chris1503@naver.com>
2023-11-15 14:45:14 +0900

    [test] implemented post and notification testing

--- backend/tests/test_schema.py
+++ backend/tests/test_schema.py
@@ -1,6 +1,8 @@
 from app.schemas.user import UserCreate, UserUpdate
 from app.schemas.post import PostCreate, PostUpdate, ImageCreate
 
+fake_uuid4 = "8e6f6dc9-bfcf-44ac-8081-58db10f4e18c"
+
 def test_schema_user_create():
     test_user_create = UserCreate(
         email="test",
@@ -28,24 +30,26 @@ def test_schema_user_update():
 
 def test_schema_post_create():
     test_post_create = PostCreate(
-        content="test",
-        image_url="test"
+        content="test"
     )
 
-    test_post_create_dict = test_post_create.create_dict("19742383-a552-4d66-9e2e-054d2b906d11")
+    test_post_create_dict = test_post_create.create_dict(fake_uuid4)
     assert test_post_create_dict['content'] == "test"
-    assert test_post_create_dict['image_url'] == "test"
-    assert test_post_create_dict['user_id'] == "19742383-a552-4d66-9e2e-054d2b906d11"
+    assert test_post_create_dict['user_id'] == fake_uuid4
 
 def test_schema_post_update():
     test_post_update = PostUpdate(
         content="test",
-        image_url="test"
+        image_url="test", 
+        origin_image_url="test", 
+        mask_image_url="test"
     )
 
     test_post_update_dict = test_post_update.create_dict() # naming consistency check necessary
     assert test_post_update_dict['content'] == "test"
     assert test_post_update_dict['image_url'] == "test"
+    assert test_post_update_dict['origin_image_url'] == "test"
+    assert test_post_update_dict['mask_image_url'] == "test"
 
 def test_schema_image_create():
     test_image_create = ImageCreate(
@@ -55,10 +59,10 @@ def test_schema_image_create():
         prompt = "test"
     )
 
-    test_image_create_dict = test_image_create.create_dict("19742383-a552-4d66-9e2e-054d2b906d11", "19742383-a552-4d66-9e2e-054d2b906d11")
+    test_image_create_dict = test_image_create.create_dict(fake_uuid4, fake_uuid4)
     assert test_image_create_dict['origin_image'] == "test"
     assert test_image_create_dict['mask_image'] == "test"
     assert test_image_create_dict['modified_image'] == "test"
     assert test_image_create_dict['prompt'] == "test"
-    assert test_image_create_dict['user_id'] == "19742383-a552-4d66-9e2e-054d2b906d11"
-    assert test_image_create_dict['post_id'] == "19742383-a552-4d66-9e2e-054d2b906d11"
+    assert test_image_create_dict['user_id'] == fake_uuid4
+    assert test_image_create_dict['post_id'] == fake_uuid4
