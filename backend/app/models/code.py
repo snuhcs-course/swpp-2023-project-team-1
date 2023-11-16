1d9be26e1de4e76eff210ac301033b2c2ceee9ed
sgeunhi <alsrb582@snu.ac.kr>
2023-11-17 00:42:14 +0900

    [feat] add timestamp to code model

--- backend/app/models/code.py
+++ backend/app/models/code.py
@@ -4,9 +4,10 @@ from sqlalchemy import String, Integer
 from app.models import Base
 from sqlalchemy.orm import Mapped, mapped_column
 from app.models.guid import GUID
+from app.models.timestamp_mixin import TimestampMixin
 
 
-class Code(Base):
+class Code(Base, TimestampMixin):
     id: Mapped[UUID4] = mapped_column(GUID, primary_key=True, default=uuid.uuid4)
     email: Mapped[str] = mapped_column(
         String(100), nullable=False, unique=True, index=True
