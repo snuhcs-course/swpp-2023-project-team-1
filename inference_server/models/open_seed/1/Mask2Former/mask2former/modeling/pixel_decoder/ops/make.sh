39ddc1dd358d5d325225028f2a1623aa9eeb3147
0jihunlee <125340163+0jihunlee@users.noreply.github.com>
2023-11-23 06:45:52 +0900

    [refactor] fix make.sh

--- inference_server/models/open_seed/1/Mask2Former/mask2former/modeling/pixel_decoder/ops/make.sh
+++ inference_server/models/open_seed/1/Mask2Former/mask2former/modeling/pixel_decoder/ops/make.sh
@@ -10,4 +10,4 @@
 # Copyright (c) Facebook, Inc. and its affiliates.
 # Modified by Bowen Cheng from https://github.com/fundamentalvision/Deformable-DETR
 
-python setup.py build install
+python3 setup.py build install
